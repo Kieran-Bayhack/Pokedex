62220f583004dba46d157fdbb6312e015b270bd9
SashHey <60923007+SashHey@users.noreply.github.com>
2024-06-04 23:38:44 +0200

    Added pokemon object

--- PokedexApp/main.js
+++ PokedexApp/main.js
@@ -1,3 +1,43 @@
 import './style.css'
-import './pokedex.js'
-import 'pokedex-api'
+//import './pokedex.js'
+//import 'pokedex-api'
+
+const pokemon = {
+  0: {
+      id: '#001',
+      name: 'Sprigatito',
+      type: 'Grass',
+      favourited: true,
+      caught: true,
+  },
+  1: {
+    id: '#002',
+    name: 'Floragato',
+    type: 'Grass',
+    favourited: false,
+    caught: false,
+  },
+  2: {
+    id: '#003',
+    name: 'Meowscarad',
+    type: 'Grass',
+    favourited: false,
+    caught: false,
+  },
+  3: {
+    id: '#004',
+    name: 'Fuecoco',
+    type: 'Fire',
+    favourited: false,
+    caught: false,
+  },
+  4: {
+    id: '#005',
+    name: 'Crocalor',
+    type: 'Fire',
+    favourited: false,
+    caught: false,
+  },
+
+}
+console.log(pokemon[0]);
